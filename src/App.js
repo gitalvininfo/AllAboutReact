584d1171ca6dd025031b455ae69f48173fba3926
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-19 17:18:00 +0800

    done in passing prop in card

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import katie from './images/katie.png';
 
 export default function App() {
     return (
         <div>
             <Navbar />
             {/* <Hero /> */}
-            <Card />
+            <Card
+                img={katie}
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life lessons with Katie Zaferes"
+                price={136}
+            />
         </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,18 +3,18 @@ import katie from '../images/katie.png';
 import star from '../images/star.png';
 
 
-export default function Card() {
+export default function Card({ img, rating, reviewCount, country, title, price }) {
     return (
         <div className='card'>
-            <img src={katie} className='card--image' />
+            <img src={img} className='card--image' />
             <div className='card--stats'>
-                <img src={star} className='card--star'/>
-                <span>5.0</span>
-                <span className='gray'>(6)</span>
-                <span className='gray'>USA</span>
+                <img src={star} className='card--star' />
+                <span>{rating}</span>
+                <span className='gray'>{reviewCount}</span>
+                <span className='gray'>{country}</span>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p> <span><b>From $136</b></span> / person</p>
+            <p>{title}</p>
+            <p> <span><b>From ${price}</b></span> / person</p>
         </div>
     )
 }
