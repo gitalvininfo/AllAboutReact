da36edc32a6101fff4aa531ab94777391409bb17
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-19 16:59:59 +0800

    done in contact component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
-
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Card from "./components/Card"
+import React from "react";
+import Contact from "./components/Contact";
 
 export default function App() {
     return (
-        <div>
-            <Navbar />
-            {/* <Hero /> */}
-            <Card />
+        <div className="contacts">
+            <Contact name="Mr. Whiskerson" phone="123" email="cat@meow1.com"/>
+            <Contact name="Mr. Whiskerson" phone="456" email="cat@meow2.com"/>
+            <Contact name="Mr. Whiskerson" phone="789" email="cat@meow3.com"/>
+            <Contact name="Mr. Whiskerson" phone="012" email="cat@meow4.com"/>
         </div>
     )
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Contact({ name, phone, email }) {
+    console.log({ name, phone, email })
+    return (
+        <div className="contact-card">
+            {/* <img src="./images/mr-whiskerson.png"/> */}
+            <h3>{name}</h3>
+            <div className="info-group">
+                {/* <img src="./images/phone-icon.png" /> */}
+                <p>{phone}</p>
+            </div>
+            <div className="info-group">
+                {/* <img src="./images/mail-icon.png" /> */}
+                <p>{email}</p>
+            </div>
+        </div>
+    )
+}
